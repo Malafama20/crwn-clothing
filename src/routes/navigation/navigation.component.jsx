7f36b5b1a663a287e5b9644b3544bf21540656e7
Malafama20 <omarzenlab@gmail.com>
2023-02-05 22:05:52 -0600

    React Router features were installed and initialized in all Components

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import './navigation.styles.scss'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+
+const Navigation = () => {
+  return(
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <CrwnLogo className='logo'/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+    
+  )
+}
+
+export default Navigation;
